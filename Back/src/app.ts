954dd181c29237fa956a9a3f20e1c04059831ff3
lucascortesc <ccordeiro.lucas@gmail.com>
2022-11-15 01:08:19 -0300

    feat: errors middleware

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,10 +1,13 @@
 import cors from "cors";
-import express from "express";
+import express, { application } from "express";
 import "express-async-errors";
+import { handleAppErrorMiddleware } from "./middlewares/handleAppError.middleware";
 
 const app = express();
 
 app.use(express.json);
 app.use(cors());
+
+app.use(handleAppErrorMiddleware);
 
 export default app;
