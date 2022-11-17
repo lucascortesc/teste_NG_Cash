f59fd4c959542c0628bc48ec2b4e6d67af8fa5a3
lucascortesc <ccordeiro.lucas@gmail.com>
2022-11-16 23:56:14 -0300

    feat: GlobalStyle

---
+++ Front/src/Styles/index.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+  
+  font-family: 'IBM Plex Sans', sans-serif;;
+}
+
+button {
+ cursor: pointer;
+}
+
+`;
